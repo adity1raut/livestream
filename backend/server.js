c813ca1557e7fa5d83735fc222d2abc43205e7a4
adity1raut <araut7798@gmail.com>
2025-09-18 19:34:40 +0530

    Solve the Confict
    Signed-off-by: adity1raut <araut7798@gmail.com>

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
   }),
 );
@@ -32,7 +32,7 @@ app.use(router);
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     methods: ["GET", "POST"],
     credentials: true,
   },
